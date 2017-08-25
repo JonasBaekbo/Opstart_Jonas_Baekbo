f904ee661414c2d392f90a9b142f334f0b128bf4
JonasBaekbo <31005038+JonasBaekbo@users.noreply.github.com>
2017-08-25 08:37:10 +0200

    Arrays/Lister

---
+++ arrays/arrays.js
@@ -0,0 +1,28 @@
+//opgave 1.1
+var cars =["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
+//____________________________________________________
+
+//opgave 1.2
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars[1];
+//____________________________________________________
+
+//opgave 1.3
+var cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+document.getElementById("demo").innerHTML = cars;
+//____________________________________________________
+
+//opgave 1.4
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars.length;
+//____________________________________________________
+
+//opgave 1.5
+var cars = ["Saab", "Volvo", "BMW"];
+cars.push("Mercedes");
+document.getElementById("demo").innerHTML = cars;
+//____________________________________________________
+
+//opgave 2 i filen arrays2.js
---
+++ arrays/arrays2.js
@@ -0,0 +1,7 @@
+//opgave 2
+var personer = [
+    ['Tobias Brage', 'Nickolas Kristensen'],
+    ['Alexander Jensen', 'Emil Berg'],
+    ['Mikkel Jacobsen', 'Natasja Andersen']
+];
+console.log(personer);
